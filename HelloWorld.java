6dc6d2eefde5d42b46621ce4d86483ac8db96992
pavani-wati <129252997+pavani-wati@users.noreply.github.com>
2025-02-15 19:32:00 +0530

    Jenkins Poll SCM changes to test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args) {
-		System.out.println("Hello Java program and welcome to Jenkins Job");
+		System.out.println("Hello Java program and welcome to Jenkins Job using Jenkins Poll SCM");
 	}
 }
